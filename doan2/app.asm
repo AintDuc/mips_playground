1149661ca8d3753be041a0d8589b3b6297b46bc4
Trinh Cao Van Duc <44729657+PanAngelosk@users.noreply.github.com>
2020-06-25 11:36:53 +0700

    handle user input, created menu

--- doan2/app.asm
+++ doan2/app.asm
@@ -1,25 +1,38 @@
 .data
-
-# n = s0
-# i = t0
 	a: .word 5
 	space: .asciiz " "
 	drop: .asciiz "\n"
-	mess: .asciiz "\nApp done!"
+	mess: .asciiz "Thank you!"
 
-	wrongInputMess: .asciiz "Enter input again!\n"
-	rightInputMess: .asciiz "Input accepted!\n"
+
 
 	arr: .space 3996
-	askMess: .asciiz "How many elements in the array?\n"
+
+	enterN: .asciiz "Enter n: "
+	enterElement: .asciiz "Enter element: "
+
+
+	printElements: .asciiz "1.Print all elements\n"
+	sumElements:.asciiz "2.Sum all elements\n"
+	printPrimeEle: .asciiz"3.Print prime elements\n"
+	maxElement: .asciiz"4.Print element which maximum value\n"
+	findElement:.asciiz "5.Find element which is x value\n"
+	exitProgram:.asciiz"6.Exit\n"
+	
+	askFeature: .asciiz "Choose: "
+	askChooseAgain: .asciiz "Choose again: "
+		
 
 .globl main
 .text
 	# start app
 	main:
-		jal ask_user
-		jal get_size_arr
-		jal check_size
+		jal menu
+		#jal enter_n
+		
+		#jal ask_user
+		#jal get_size_arr
+		#jal check_size
 
 		#jal check_user_input
 		#jal log_input_user
@@ -28,6 +41,7 @@
 		
 	# end of app
 	jal system_pause
+		
 
 	
 
@@ -64,10 +78,16 @@
 
 	# function: drop down - drop down the line
 	drop_down:
+		sub $sp,$sp,4
+		sw $ra,4($sp)
+
 		li $v0,4
 		la $a0,drop
 		syscall
-#		jr $ra
+
+		lw $ra,4($sp)
+		add $sp,$sp,4
+		jr $ra
 
 	# function: get_n_arr - get size of arr
 	get_size_arr:
@@ -76,51 +96,168 @@
 		move $s0,$v0		
 		# jr $ra
 
-	# function: ask_user - ask amount of elements in array
-	ask_user:
-		li $v0,4
-		la $a0,askMess
-		syscall
-		jr $ra
+
+		
+	# function: enter_n - get size of array from user
+	enter_n:
+		sub $sp,$sp,4
+		sw $ra,4($sp)
+		
 	
 
-	# function: log_input_user - print value from user input
-	log_input_user:
-		li $v0,1
-		move $a0,$t2
-		syscall
+		jal handle_n
+
+
+		lw $ra,4($sp)
+		add $sp,$sp,4
 		jr $ra
 
-	# function: check_size - check size array
-	# 0 < n < 1000
-	check_size:
-		# if (n<0 || n>1000) - input again
-		blt $s0,0,if
-		bgt $s0,1000,if
+	# function: handle_n - handle array
+	handle_n:
+		sub $sp,$sp,4
+		sw $ra,4($sp)
+
+		enter_n_again:
+			li $v0,4
+			la $a0,enterN
+			syscall
+
+			li $v0,5
+			syscall		
+
+		blt $v0,0,enter_n_again
+		bgt $v0,1000,enter_n_again
+
+		lw $ra,4($sp)
+		add $sp,$sp,4
+		jr $ra
+
+
+	# function: enter_element - print get element
+	enter_element:
+		sub $sp,$sp,4
+		sw $ra,4($sp)
+
+		li $v0,4 
+		la $a0,enterElement
+		syscall
+
+		lw $ra,4($sp)
+		add $sp,$sp,4
+		jr $ra
+
+	# function: get_element - get element
+	get_element:
+		sub $sp,$sp,4
+		sw $ra,4($sp)
+
+
+
+		lw $ra,4($sp)
+		add $sp,$sp,4
+		jr $ra
+
+	# function: menu - print menu
+	menu:
+		sub $sp,$sp,4
+		sw $ra,4($sp)		
+
+		li $t0,1
+		do:
+			jal log_feature
+
+			addi $t0,$t0,1			
+
+			blt $t0,2,do
+		
+		lw $ra,4($sp)		
+		add $sp,$sp,4
+		jr $ra
+
+	
+
+	# function: print all feature
+	log_feature:
+
+		sub $sp,$sp,4
+		sw $ra,4($sp)
+
+		li $v0,4
+		la $a0,printElements
+		syscall
+
+		li $v0,4
+		la $a0,sumElements
+		syscall
+
+		li $v0,4
+		la $a0,printPrimeEle
+		syscall
+
+		li $v0,4
+		la $a0,maxElement
+		syscall
+
+		li $v0,4
+		la $a0,findElement
+		syscall
+
+		li $v0,4
+		la $a0,exitProgram
+		syscall
+
+		jal ask_feature
+
+		jal drop_down
+
+		lw $ra,4($sp)
+		add $sp,$sp,4		
+		jr $ra
+
+	# function: ask_feature - ask user to choose feature
+	ask_feature:
+		sub $sp,$sp,4
+		sw $ra,4($sp)
+
+		
+	
+		jal handle_user_chosen
+		
+
+		lw $ra,4($sp)
+		add $sp,$sp,4
+		jr $ra
+
+	# function: handle_user_chosen - 1 to 6
+	handle_user_chosen:
+		sub $sp,$sp,4
+		sw $ra,4($sp)
+
+		again:
+			li $v0,4
+			la $a0,askFeature
+			syscall
+			
+			li $v0,5
+			syscall
+			move $s0,$v0
+
+		# handle overflow 
+		blt $s0,1,again
+		bgt $s0,6,again
+		# feature 6
+		beq $v0,6,ft_6
+		ft_6:
+			jal system_pause
+
+	
+
+
+		lw $ra,4($sp)
+		add $sp,$sp,4
+		jr $ra
 	
 		
-		# else 
-		jal right_input_mess
-
-		if: 
-			jal wrong_input_mess
-			jal ask_user
-		jr $ra
-		
-	# function: print wrong input message
-	wrong_input_mess:
-		li $v0,4
-		la $a0,wrongInputMess
-		syscall
-		jr $ra
-
-
-	# function: print right input message
-	right_input_mess:
-		li $v0,4
-		la $a0,rightInputMess
-		syscall
-		jr $ra
 		
 		
 	
